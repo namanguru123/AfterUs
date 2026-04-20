1ecf815e26a11d69afed2b364a99450fa723624f
namanguru123 <namansharma7898@gmail.com>
2026-04-21 01:59:26 +0530

    Fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:3000",
       "https://after-us.vercel.app",
     ],
     credentials: true,
