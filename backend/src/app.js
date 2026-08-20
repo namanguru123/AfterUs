21b2420f2f572c56daf1cb5adab2f05eb2491629
namanguru123 <namansharma7898@gmail.com>
2026-08-20 17:12:47 +0530

    shifted the deployment from aws to render

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://localhost:80",
+      "https://after-us.vercel.app/"
       "http://13.62.19.92"
     ],
     credentials: true,
