e6228b23ab72100ec48de367eb6511e322576663
namanguru123 <namansharma7898@gmail.com>
2026-08-20 17:27:25 +0530

    wrong route fixed

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,5 +7,5 @@ const API = axios.create({
   },
 });
 
-export const registerUser = (data) => API.post("/api/auth/register", data);
-export const loginUser = (data) => API.post("/api/auth/login", data);
+export const registerUser = (data) => API.post("/auth/register", data);
+export const loginUser = (data) => API.post("/auth/login", data);
